6064701128d424a97d84755dd55583ec057ffbf9
Sarah Dreischer <sarah.dreischer@gmail.com>
2020-09-06 15:05:19 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { TodoViewComponent } from './todo-view/todo-view.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+
+import { TodoReducer } from './todo.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoViewComponent,
+    TodoAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ todos: TodoReducer }, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
